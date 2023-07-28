cd6ac2910519caf5bd5b7dca27c1f648f4d6ce30
CaroBriguez <caro13briguez@gmail.com>
2023-07-28 18:06:54 -0300

    .

---
+++ Laboratorio/JavaScript/index.js
@@ -0,0 +1,9 @@
+/*const num = "hola"
+console.log(typeof num);*/
+
+const prompt=require("prompt-sync")({sigint:true});
+
+let num1 = prompt("Ingrese un valor:");
+
+console.log(num1);
+console.log()
