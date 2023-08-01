57a4b3226e01bd022ef5ad460bb1ea8b74b07104
CaroBriguez <caro13briguez@gmail.com>
2023-08-01 11:05:14 -0300

    clase 5

---
+++ Laboratorio/JavaScript/Clase5.js
@@ -0,0 +1,47 @@
+/*Agregar 10 onjetos personas con diferentes datos.
+
+1)Filtrado por Nombre(Coincidencia)(Solicitar al usuario el nombre a buscar)
+2)Mostrar el promedio de las edades
+*/
+
+const prompt=require("prompt-sync")({sigint:true});
+
+let Arraypersonas = []
+const personas = (nombre,lastname,edad,direccion,status,orientacion) => {
+    let person = {
+        nombre,
+        lastname,
+        edad,
+        direccion,
+        status};
+    if(orientacion){
+        Arraypersonas.push(person);
+    }
+    else{
+        Arraypersonas.unshift(person);
+    }}
+
+personas("Carolina", "Briguez", 21, "MDP",true,true)
+personas("Camila", "Zarlenga", 18, "MDP",true,true)
+personas("Cata", "Martinez", 16, "MDP",true,true)
+personas("Lucia", "Santamaria", 20, "MDP",true,true)
+personas("Sofia", "Sancehz", 22, "MDP",true,true)
+personas("Martina", "Sarasola", 21, "MDP",true,true)
+personas("Julieta", "Gomez", 19, "MDP",true,true)
+personas("Candela", "Gimenez", 25, "MDP",true,true)
+personas("Sol", "Lopez", 14, "MDP",true,true)
+personas("Maria", "Perez", 15, "MDP",true,true)
+
+/* 1)*/
+let DatapersonasFilter = Arraypersonas.filter((personas) =>{ 
+    return personas.nombre === "Carolina"
+});
+
+
+console.table(DatapersonasFilter)
+console.log("**************");
+console.log(Arraypersonas)
+
+/* 2) */
+
+let promedioEdades 
--- Laboratorio/JavaScript/index.js
+++ Laboratorio/JavaScript/index.js
@@ -3,7 +3,73 @@ console.log(typeof num);*/
 
 const prompt=require("prompt-sync")({sigint:true});
 
-let num1 = prompt("Ingrese un valor:");
+/*let num1 = prompt("Ingrese un valor:");
 
 console.log(num1);
-console.log()
+console.log()*/
+
+
+/* Actividad Array */ 
+
+let Arraypersonas = []
+
+
+/* Funcion Flecha*/
+const personas = (nombre,lastname,edad,direccion,status,orientacion) => {
+    let person = {
+        nombre,
+        lastname,
+        edad,
+        direccion,
+        status
+    };
+    if(orientacion){
+        Arraypersonas.push(person);
+    }
+    else{
+        Arraypersonas.unshift(person);
+    }
+}
+/* Funcion*/
+personas("Carolina", "Briguez", 21, "MDP",true,true) 
+
+/*for(i=1; i <=2;i++){
+    let nombre = prompt("Ingrese el Nombre");
+    let lastname = prompt("Ingrese el Apellido");
+    let edad = parseInt(prompt("Ingrese la edad"));
+    let direccion = prompt("Ingrese la direccion");
+    let status = prompt("Ingrese su Status");
+    personas(nombre,lastname,edad,direccion,status,true)
+    console.log("**************");
+}*/
+/*Filter*/ 
+let DatapersonasFilter = Arraypersonas.filter((person) =>{
+    return person.edad > 15;
+});
+
+
+console.log(DatapersonasFilter)
+console.log("**************");
+console.log(Arraypersonas)
+
+/* MAP*/
+
+let datapersonaEdad = Arraypersonas.map((element)=>{ 
+    if(element.edad < 18){
+        return "Menor de Edad";
+    }
+    else{
+        return "Mayor de edad";
+    }
+})
+/*ORDENA DE MANERA ASCENDENTE O DESCENDENTE LA EDAD, se hizo la funcion felcha en el mismo console */
+console.table(Arraypersonas.sort((a,b)=> {
+    return a.edad - b.edad
+}))
+
+console.table (Arraypersonas.sort((a,b)=>{
+    return b.edad - a.edad
+}))
+
+/* Include = ver si esta incluido ese valor, devuelve true o false*/
+Arraypersonas.includes("Carolina");
