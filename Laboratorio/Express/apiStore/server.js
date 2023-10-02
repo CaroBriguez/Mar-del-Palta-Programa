10841902b24cea390a962c1cd56a818af7d1df3d
CaroBriguez <caro13briguez@gmail.com>
2023-10-02 19:49:09 -0300

    .

--- Laboratorio/Express/apiStore/server.js
+++ Laboratorio/Express/apiStore/server.js
@@ -2,4 +2,12 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get
+const userRoute = require('./routers/user.router')
+
+app.get('/'(req,res)=>{
+    res.json({welcome: "WELCOME"});
+})
+
+app.listen(port,()=>{
+    console.log("Listen port: https://localhost")
+})
